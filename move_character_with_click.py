44d366115e48492386f1fde6a29e6ab9540cddcc
D4LGONA(samsung) <laff3y2297@gmail.com>
2023-10-01 02:50:17 +0900

    커서 이미지가 커서를 따라가요

--- move_character_with_click.py
+++ move_character_with_click.py
@@ -9,34 +9,49 @@ cursor = load_image('hand_arrow.png')
 
 
 def handle_events():
-    global running
-    global x, y
+    global running, cursorX, cursorY
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
         elif event.type == SDL_MOUSEMOTION:
-            x, y = event.x, TUK_HEIGHT - 1 - event.y
+            cursorX, cursorY = event.x, TUK_HEIGHT - 1 - event.y
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
     pass
 
 def draw():
+    global points, frame, x, y, cursorX, cursorY
     clear_canvas()
     TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
-    character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+    if points:
+        for i in points:
+            cursor.draw(i[0], i[1])
+    if dirX > 0:
+        character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+    else:
+        character.clip_draw(frame * 100, 100 * 0, 100, 100, x, y)
+    cursor.draw(cursorX, cursorY)
     update_canvas()
     frame = (frame + 1) % 8
 
+def move():
+    print('움직여야 해')
 
 running = True
 x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
+cursorX, cursorY = 0, 0
 points = []
 frame = 0
+dirX, dirY = 0, 0
 hide_cursor()
 
 while running:
-    
+    if points: # 배열이 비어있지 않다면
+        move()
+
+    draw()
+
 
     handle_events()
 
